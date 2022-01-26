d46cbc107366e632ba0bff9f1546bea0d8116a4a
MrVipe <mr.vi@mail.ru>
2022-01-26 23:10:26 +0400

    Добавьте файлы проекта.

---
+++ Less2_1/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Less2_1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите минимальную температару");
+            var Tmin = Console.ReadLine();
+            Console.WriteLine("Введите максимальную температару");
+            var Tmax = Console.ReadLine();
+            var Tsredn = (Convert.ToDouble(Tmax) + Convert.ToDouble(Tmin)) / 2;
+            Console.WriteLine("Средняя температура = " + Tsredn);
+            Console.ReadKey();
+        }
+    }
+}
